a60459d49683a03be7f8d57b23f412e40961c5ee
davidwt5 <davidwtan75@gmail.com>
2021-07-28 16:48:24 +1000

    feat: inputting numbers

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function operate(a, b, ) {
+const MAX_DISPLAY_LENGTH = 20;
 
-}
+const display = document.querySelector('.display');
+const numberBtns = document.querySelectorAll('.number');
+numberBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        if(display.innerText === '0')
+            display.innerText = e.target.innerText;
+        else if(display.innerText.length <= MAX_DISPLAY_LENGTH)
+            display.innerText += e.target.innerText;
+    });
+});
