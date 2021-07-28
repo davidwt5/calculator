a6a952c28209a757b331711938ee4d0683765ee7
davidwt5 <davidwtan75@gmail.com>
2021-07-28 16:51:35 +1000

    made reset button functinal

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const MAX_DISPLAY_LENGTH = 20;
-
 const display = document.querySelector('.display');
+
 const numberBtns = document.querySelectorAll('.number');
 numberBtns.forEach(btn => {
     btn.addEventListener('click', e => {
@@ -9,4 +9,9 @@ numberBtns.forEach(btn => {
         else if(display.innerText.length <= MAX_DISPLAY_LENGTH)
             display.innerText += e.target.innerText;
     });
+});
+
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', e => {
+    display.innerText = 0;
 });
